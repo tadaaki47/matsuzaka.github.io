07782d07ed4beaf34f557b788e309aa3e8faa479
tadaaki47 <tadaaki.47.m@gmail.com>
2026-01-31 20:17:13 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const myData = {
+    publications: [
+        {
+            category: "Journal Article / Nature Communications",
+            title: "Neural mechanisms of collective decision making...",
+            info: "First Author, 2024. [DOI: 10.1038/s41467-024-XXXXX-X]",
+            link: "#"
+        }
+        // 次からはここを増やすだけでOK！
+    ],
+    grants: [
+        { year: "2023", name: "JSPS Grant-in-Aid for Early-Career Scientists" }
+    ]
+};
